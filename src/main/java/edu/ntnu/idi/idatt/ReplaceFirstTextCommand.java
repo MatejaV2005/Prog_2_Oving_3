da7c887c93175dde08cdff847d932642f32a604b
matejav <matejav@stud.ntnu.no>
2025-01-29 18:30:41 +0100

    task 3: finished ReplaceFirstTextCommand. task 3 is finished

---
+++ src/main/java/edu/ntnu/idi/idatt/ReplaceFirstTextCommand.java
@@ -0,0 +1,14 @@
+package edu.ntnu.idi.idatt;
+
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
+
+  ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement); // extends the Parent-class constructor
+  }
+
+  // Implementation of replacing first instance of target word
+  @Override
+  public String execute(String text) {
+    return text.replaceFirst(target, replacement);
+  }
+}
