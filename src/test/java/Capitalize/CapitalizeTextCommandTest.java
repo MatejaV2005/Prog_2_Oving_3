bb5bf766570cd4b01f384edef46f2c0099bf24f8
matejav <matejav@stud.ntnu.no>
2025-02-03 10:39:07 +0100

    Finished test for CapitalizeTextCommand, all tests passed

---
+++ src/test/java/Capitalize/CapitalizeTextCommandTest.java
@@ -0,0 +1,66 @@
+package Capitalize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+import edu.ntnu.idi.idatt.capitalize.CapitalizeTextCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CapitalizeTextCommandTest {
+
+    private CapitalizeTextCommand command;
+
+    @BeforeEach
+    void setUp() {
+        command = new CapitalizeTextCommand();
+    }
+
+    @Test
+    void testCapitalizeNormalText() {
+        String input = "hello world";
+        String expected = "Hello world";
+        assertEquals(expected, command.execute(input));
+    }
+
+    @Test
+    void testOneOrMultipleCapitalLetter() {
+        String input1 = "hello World";
+        String expected1 = "Hello World";
+
+        String input2 = "HELLO world";
+        String expected2 = "HELLO World";
+        assertEquals(expected1, command.execute(input1));
+    }
+
+    @Test
+    void testNumbersInString() {
+        String input = "m4t3ja 7asic";
+        String expected = "M4t3ja 7asic";
+        assertEquals(expected, command.execute(input));
+    }
+
+
+    //NEGATIVE TEST -------------------------------------------
+    @Test
+    void testNullString() {
+        String input = null;
+        assertThrows(IllegalArgumentException.class, () -> {
+            command.execute(input);
+        });
+    }
+
+    @Test
+    void testBlankString() {
+        String input = "";
+        assertThrows(IllegalArgumentException.class, () -> {
+            command.execute(input);
+        });
+    }
+
+
+
+
+
+
+}
