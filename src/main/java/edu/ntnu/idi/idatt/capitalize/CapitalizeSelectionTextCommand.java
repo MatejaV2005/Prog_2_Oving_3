dca950cd645e61d40c1ade43c3e30458db540a54
MatejaV2005 <matejav@stud.ntnu.no>
2025-02-05 13:47:24 +0100

    forgot to commit. integrated exceptionhandling in CapitalizeSelectionTextCommand

--- src/main/java/edu/ntnu/idi/idatt/capitalize/CapitalizeSelectionTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/capitalize/CapitalizeSelectionTextCommand.java
@@ -4,6 +4,10 @@ public class CapitalizeSelectionTextCommand extends CapitalizeTextCommand {
   String selection;
 
   public CapitalizeSelectionTextCommand(String selection) {
+    if (selection == null || selection.isBlank()) {
+      throw new IllegalArgumentException("selection cant be null or blank");
+    }
+
     this.selection = selection;
   }
 
@@ -13,6 +17,9 @@ public class CapitalizeSelectionTextCommand extends CapitalizeTextCommand {
 
   @Override
   public String execute(String text) {
+    if (!text.contains(getSelection())) {
+      throw new IllegalArgumentException("chosen selection is not in the text");
+    }
     return text.replace(selection, selection.substring(0, 1).toUpperCase() + selection.substring(1));
   }
 }
